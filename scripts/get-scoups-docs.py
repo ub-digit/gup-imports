6757af1c6a3b5089a33d874c49aea96356a9f48d
Per Larsson <per.larsson@ub.gu.se>
2025-01-29 15:35:11 +0100

    GUP-1522 Use the Scoups Abstract API to retrieve all authors and affiliations if there are more than 100 authors

--- scripts/get-scoups-docs.py
+++ scripts/get-scoups-docs.py
@@ -5,6 +5,78 @@ import os
 import argparse
 from datetime import date, timedelta, datetime
 
+def get_affiliations(affiliations):
+  # Make sure affiliations is a list
+  if not isinstance(affiliations, list):
+    affiliations = [affiliations]
+  # Loop through all affiliations and create a list of affiliations
+  all_affiliations = []
+  for affiliation in affiliations:
+    affiliation_dict = {}
+    affiliation_dict["@_fa"] = "true" # Always true? (Not used)
+    affiliation_dict["$"] = affiliation["@id"]
+    all_affiliations.append(affiliation_dict)
+  return all_affiliations
+
+def get_full_info(scopus_id, api_key):
+  # Use the Abstract API to get all authors and affiliations
+  base_url = 'https://api.elsevier.com/content/abstract/scopus_id/'
+  params = {'apiKey': api_key, 'httpAccept': 'application/json', 'view': 'FULL'}
+  response = requests.get(base_url + scopus_id, params)
+  content = response.content
+  raw_json = json.loads(content)
+  authors = raw_json["abstracts-retrieval-response"]["authors"]["author"]
+  # Loop through all authors and create a new list of authors
+  all_authors = []
+  for author in authors:
+    author_dict = {}
+    author_dict["@_fa"] = author["@_fa"]
+    author_dict["@seq"] = author["@seq"]
+    author_dict["author-url"] = author["author-url"]
+    author_dict["authid"] = author["@auid"]
+    author_dict["authname"] = author["ce:indexed-name"]
+    author_dict["surname"] = author["ce:surname"]
+    # Check if the author has a given name
+    if "ce:given-name" in author:
+      author_dict["given-name"] = author["ce:given-name"]
+    # Check if the author has initials
+    if "ce:initials" in author:
+      author_dict["initials"] = author["ce:initials"]
+    # Check if the author has an affiliation
+    if "affiliation" in author:
+      author_dict["afid"] = get_affiliations(author["affiliation"])
+    all_authors.append(author_dict)
+
+  # Loop through all affiliations and create a new list of affiliations
+  all_affiliations = []
+  affiliations = raw_json["abstracts-retrieval-response"]["affiliation"]
+  for affiliation in affiliations:
+    affiliation_dict = {}
+    affiliation_dict["@_fa"] = "true" # Always true? (Not used)
+    affiliation_dict["affiliation-url"] = affiliation["@href"]
+    affiliation_dict["afid"] = affiliation["@id"]
+    affiliation_dict["affilname"] = affiliation["affilname"]
+    affiliation_dict["affiliation-city"] = affiliation["affiliation-city"]
+    affiliation_dict["affiliation-country"] = affiliation["affiliation-country"]
+    all_affiliations.append(affiliation_dict)
+
+  # To get the orcid for the author, if any, we need to search in another part of the response
+  author_group = raw_json["abstracts-retrieval-response"]["item"]["bibrecord"]["head"]["author-group"]
+  # This is a list of two objects, one with the key "affiliation" and one with the key "author".
+  # The "affiliation" object can be ignored.
+  # The "author" object is a list of authors, where the orcid can be found
+  # If the author has an orcid, it will be in the "@orcid" key
+  # If an ordid is found it will be added to the corresponding author in the all_authors list
+  for author in author_group:
+    if "author" in author:
+      for author_info in author["author"]:
+        for author in all_authors:
+          if author_info["@seq"] == author["@seq"]:
+            if "@orcid" in author_info:
+              author["orcid"] = author_info["@orcid"]
+
+  return {"authors": all_authors, "affiliations": all_affiliations}
+
 base_url = 'https://api.elsevier.com/content/search/scopus'
 base_query = '(AF-ID("Goteborgs Universitet" 60016437) OR AF-ID("Sahlgrenska Academy" 60027675) OR AF-ID("Goteborgs universitet Institutionen for biomedicin" 60079626) OR AF-ID("Tjarno Marine Biological Laboratory" 60055702) OR AF-ID("[No Affiliation ID found" 60008491) OR AF-ID("Goteborgs Universitet Institute of Clinical Sciences" 60004831) OR AF-ID("[No Affiliation ID found" 60010505) OR AF-ID("Hogskolan for Design och Konsthantverk" 60005288))'
 
@@ -59,6 +131,18 @@ while (first or start < total):
   docs = raw_json["search-results"]["entry"]
   for doc in docs:
     scopus_id = doc["dc:identifier"].split("SCOPUS_ID:",1)[1]
+    # Special handling for authors, check if total number of authors is more than 100
+    if int(doc["author-count"]["@total"]) > 100:
+      print("More than 100 authors for document with Scopus Id " + scopus_id)
+      full_info = get_full_info(scopus_id, args.apikey)
+      all_authors = full_info["authors"]
+      all_affiliations = full_info["affiliations"]
+      # Rename the author and affiliation keys to author_org and affiliation_org
+      doc["author_org"] = doc["author"]
+      doc["affiliation_org"] = doc["affiliation"]
+      # Replace the authors and affiliations with the full info
+      doc["author"] = all_authors
+      doc["affiliation"] = all_affiliations
     print("Saving document with Scopus Id " + scopus_id)
     with open(f"{args.output}/{args.date}/{scopus_id}.json", "w") as f:
       json.dump(doc, f)
